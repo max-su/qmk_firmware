cc9acad04bde8c511edaab7f1aba9f87f46613e2
Max Su <max@maxsu.dev>
2019-07-10 01:40:53 -0400

    update keymap for linux

--- keyboards/ergodox_ez/keymaps/max_su/keymap.c
+++ keyboards/ergodox_ez/keymaps/max_su/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
-#define BASE_OSX 0 // Default layer (Mac OSX)
+#define BASE_LINUX 0 // Default layer (Mac OSX)
 #define BASE_WINDOWS 1 // Default layer (Windows)
 #define SYMB 2 // Symbols
 #define ARRW 3 // HJKL => Arrow keys, fn keys
@@ -41,30 +41,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |ace   |  L3  |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
-[BASE_OSX] = LAYOUT_ergodox(
+[BASE_LINUX] = LAYOUT_ergodox(
   // Left Hand.
-  KC_CAPS, KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    DF(BASE_WINDOWS),
+  KC_ESC,  KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    DF(BASE_WINDOWS),
   KC_TAB,  KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    MO(ARRW),
-  KC_ESC,  KC_A,        KC_S,          LT(NUMB, KC_D),    LT(ARRW, KC_F),    KC_G,
+  KC_CAPS, KC_A,        KC_S,          LT(NUMB, KC_D),    LT(ARRW, KC_F),    KC_G,
   KC_LSFT, KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    MO(SYMB),
   KC_MINS, KC_PLUS,     KC_LCTRL,       KC_LALT,  KC_LGUI,
-                                                                KC_MRWD,  KC_MFFD,
+                                                                KC_MPRV,  KC_MNXT,
                                                                           KC_MPLY,
                                                          KC_SPC, KC_BSPC, MO(NUMB),
   // Right Hand.
-  KC_LGUI,  KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
+  KC_PSCR,  KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
   MO(ARRW),  KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   KC_H,     KC_J,    KC_K,    KC_L,    LT(ARRW, KC_SCLN), KC_QUOT,
   MO(SYMB), KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH, KC_RSFT,
   KC_LGUI,  KC_LALT, KC_LCTRL, KC_LEFT, KC_RGHT,
-  KC__VOLDOWN, KC__VOLUP,
-  KC__MUTE,
+  KC_VOLD, KC_VOLU,
+  KC_MUTE,
   TG(ARRW), KC_TAB, KC_ENT
 ),
 
 [BASE_WINDOWS] = LAYOUT_ergodox(
   // Left Hand.
-  KC_CAPS, KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    DF(BASE_OSX),
+  KC_CAPS, KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    DF(BASE_LINUX),
   KC_TAB,  KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    MO(ARRW),
   KC_ESC,  KC_A,        KC_S,          LT(NUMB, KC_D),    LT(ARRW, KC_F),    KC_G,
   KC_LSFT, KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    MO(SYMB),
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                           KC_MPLY,
                                                          KC_SPC, KC_BSPC, MO(NUMB),
   // Right Hand.
-  KC_LCTRL,  KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
+  KC_PSCR,  KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
   MO(ARRW),  KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   KC_H,     KC_J,    KC_K,    KC_L,    LT(ARRW, KC_SCLN), KC_QUOT,
   MO(SYMB), KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH, KC_RSFT,
@@ -315,3 +315,24 @@ uint32_t layer_state_set_user(uint32_t state) {
 
   return state;
 };
+
+/* [BASE_OSX] = LAYOUT_ergodox( */
+/*   // Left Hand. */
+/*   KC_CAPS, KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    DF(BASE_WINDOWS), */
+/*   KC_TAB,  KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    MO(ARRW), */
+/*   KC_ESC,  KC_A,        KC_S,          LT(NUMB, KC_D),    LT(ARRW, KC_F),    KC_G, */
+/*   KC_LSFT, KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    MO(SYMB), */
+/*   KC_MINS, KC_PLUS,     KC_LCTRL,       KC_LALT,  KC_LGUI, */
+/*                                                                 KC_MRWD,  KC_MFFD, */
+/*                                                                           KC_MPLY, */
+/*                                                          KC_SPC, KC_BSPC, MO(NUMB), */
+/*   // Right Hand. */
+/*   KC_LGUI,  KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS, */
+/*   MO(ARRW),  KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS, */
+/*   KC_H,     KC_J,    KC_K,    KC_L,    LT(ARRW, KC_SCLN), KC_QUOT, */
+/*   MO(SYMB), KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH, KC_RSFT, */
+/*   KC_LGUI,  KC_LALT, KC_LCTRL, KC_LEFT, KC_RGHT, */
+/*   KC__VOLDOWN, KC__VOLUP, */
+/*   KC__MUTE, */
+/*   TG(ARRW), KC_TAB, KC_ENT */
+/* ), */
